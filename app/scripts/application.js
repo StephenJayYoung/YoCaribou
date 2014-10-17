90747baebccec172bd3f7e34923fef6505208860
Stephen Young <StephenJayYoung@gmail.com>
2014-10-17 10:30:55 -0700

    added a cheeseburger picture to the page

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -16,35 +16,5 @@ Yocaribou.CheeseburgerRoute = Ember.Route.extend({
 });
 
 Yocaribou.CheeseburgerController = Ember.ObjectController.extend({
-	moreStuff: 'more cheeseburgers',
-	evenMoreStuff: function() {
-		return 'the last cheeseburger in the world';
-	}.property()
-})
-
-
-
-
-
-
-
-// var near = $('#target input').val();
-// var data = {
-// 		near: near,
-// 		section: 'food',
-// 		venuePhotos: '1',
-
-// 		v: '20141002',
-// 		client_id: 'THF0PIAQPEPL3UJZJGVVXKL5S1FM4P54MGZARXUFJ1ZGBENP',
-// 		client_secret: 'PAY5FKCOQB4NI0CSL5XDXNL1AOA2CA2CWTACTYILINBMK4S0'
-// 	};
-
-// var url = "https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=98a80bf27f25bb1381e9bea26b6282e5&per_page=500&format=json&nojsoncallback=1";
-
-// App.IndexRoute = Ember.Route.extend({
-//   model: function() {
-//     return Ember.$.ajax({
-//       type: 'GET',
-//       url: url,
-//       dataType: 'JSON'
-//     })
+	cheeseburgerPicture: 'http://sonicmenu.s3.amazonaws.com/2651348868959.13122.png'
+});
